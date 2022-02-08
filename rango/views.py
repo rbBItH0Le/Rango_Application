8877c6b56e607417afaadab75459c95e9b8dfe8a
2588151t <2588151T@student.gla.ac.uk>
2022-02-08 19:35:56 +0000

    chapter8

--- rango/views.py
+++ rango/views.py
@@ -31,6 +31,8 @@ def show_category(request, category_name_slug):
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Rohan'}
+    print(request.method)
+    print(request.user)
     return render(request, 'rango/about.html', context=context_dict)
 
 def add_category(request):
